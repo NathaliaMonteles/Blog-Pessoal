400b37ff535622ee6488285bc9637187971dda30
Nathalia Monteles <nathalia_martinsm@hotmail.com>
2024-05-21 14:10:20 -0300

    Inicio do projeto blog pessoal

---
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -0,0 +1,11 @@
+package com.generation.blogpessoal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.blogpessoal.model.Postagem;
+
+//JPaRepository - classe JPA - metodos que vão realizar query no banco
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+	//SELECT 
+
+}
