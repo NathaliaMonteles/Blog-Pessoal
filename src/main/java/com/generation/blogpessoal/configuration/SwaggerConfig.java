a54ec7a3de1574e163d0481f1e94f90d03c45310
Nathalia Monteles <nathalia_martinsm@hotmail.com>
2024-06-07 12:11:14 -0300

    Deploy do Projeto Blog Pessoal

---
+++ src/main/java/com/generation/blogpessoal/configuration/SwaggerConfig.java
@@ -0,0 +1,62 @@
+package com.generation.blogpessoal.configuration;
+
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+
+@Configuration
+public class SwaggerConfig {
+	
+	@Bean
+	OpenAPI springBlogPessoalOpenAPI() {
+		return new OpenAPI()
+				.info(new Info()
+						.title("Projeto Blog Pessoal da Nathalia")
+						.description("Projeto Blog Pessoal desenvolvido na Generation Brasil")
+						.version("v0.0.1")
+						.license(new License()
+								.name("Nathalia Monteles")
+								.url("meu site - https://www.linkedin.com/in/nathalia-martins-monteles-a42117164/"))
+							.contact(new Contact()
+								.name("Nathalia Monteles")
+								.url("")
+								.email("nathalia_martinsm@hotmail.com")))
+				.externalDocs(new ExternalDocumentation()
+						.description("Github")
+						.url("https://github.com/NathaliaMonteles/Blog-Pessoal"));
+	}
+	
+	@Bean
+	OpenApiCustomizer customerGlobalHeaderOpenApiCustomiser() {
+		
+		return openApi -> {
+			openApi.getPaths().values().forEach(pathItem -> pathItem.readOperations()
+					.forEach(operation -> {
+						
+						ApiResponses apiResponses = operation.getResponses();
+						
+						apiResponses.addApiResponse("200", createApiResponse("Sucesso!"));
+						apiResponses.addApiResponse("201", createApiResponse("Objeto Persistido!"));
+						apiResponses.addApiResponse("204", createApiResponse("Objeto Excluído!"));
+						apiResponses.addApiResponse("400", createApiResponse("Erro na Requisição!"));
+						apiResponses.addApiResponse("401", createApiResponse("Acesso não Autorizado!"));
+						apiResponses.addApiResponse("403", createApiResponse("Acesso Proibido!"));
+						apiResponses.addApiResponse("404", createApiResponse("Objeto não Encontrado!"));
+						apiResponses.addApiResponse("500", createApiResponse("Erro na Aplicação!"));
+					}));
+		};
+	}
+	
+	private ApiResponse createApiResponse(String message) {
+		return new ApiResponse().description(message);
+	}
+
+}
